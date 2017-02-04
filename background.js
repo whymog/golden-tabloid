cb05ceb2561039e8ee785b6d41b874789e801a96
Nick Cummings <nickmcummings@gmail.com>
2017-02-04 14:14:51 -0800

    Added instructions when browser icon is clicked

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 console.log("Ready for Achewood.");
 //
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   chrome.tabs.executeScript(null, {file: "app.js"});
-// });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  window.alert("An Achewood companion.\n\nArrow keys: previous/next comic\nA: view alt text");
+});
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   console.log(tabId, changeInfo, tab);
   if (changeInfo.status === 'complete' && tab.active) {
