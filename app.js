7dcff65e3f1828e60f68fd411c4222fbf8f47a29
Bryan Richter <b@chreekat.net>
2017-02-11 18:06:41 -0800

    Add R for random

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ window.addEventListener("keydown", function(e) {
   } else if (e.key.match(/^a$/i)) {
     const altText = document.querySelector("#comic_body img.comic").getAttribute("title");
     window.alert(altText);
+  } else if (e.key.match(/^r$/i)) {
+    window.location = 'http://www.ohnorobot.com/random.pl?comic=636';
   }
 });
